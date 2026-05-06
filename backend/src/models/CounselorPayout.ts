ab42af7e4c79b2ca02338ed1280e8507eb992f34
naol16 <lemesanaol93@gmail.com>
2026-05-06 21:06:08 +0300

    new change

--- backend/src/models/CounselorPayout.ts
+++ backend/src/models/CounselorPayout.ts
@@ -9,6 +9,7 @@ import {
     UpdatedAt,
 } from "sequelize-typescript";
 import { Counselor } from "./Counselor.js";
+import type { Counselor as CounselorType } from "./Counselor.js";
 
 @Table({
     tableName: "counselor_payouts",
